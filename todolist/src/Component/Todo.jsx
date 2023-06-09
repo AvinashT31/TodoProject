6c4ed89c876f7071cf2f4b00945a0b70fa58b786
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-06-09 16:26:15 +0530

    update

--- todolist/src/Component/Todo.jsx
+++ todolist/src/Component/Todo.jsx
@@ -3,12 +3,34 @@ import "../Styles/Todo.css";
 
 const Todo = () => {
 
-  const[inputData, setinputData] = useState("");
+  const [inputData, setinputData] = useState("");
   console.log(inputData, "inputData");
+
+  const [addItem, setaddItem] = useState([]);
+  console.log(addItem, "addItem");
 
   const handleclick = (e) => {
     // console.log(e.target.value);
     setinputData(e.target.value);
+  }
+
+  const AddItem = () => {
+    setaddItem([...addItem, inputData]);
+    setinputData("");
+  }
+
+  const deleteItem = (id) => {
+    // alert("working");
+    console.log(id);
+
+    const updatedItem = addItem.filter((e, index) => {
+      return index !== +id;
+    })
+    setaddItem(updatedItem);
+  }
+
+  const RemoveAllTodo = () => {
+    setaddItem([]);
   }
 
 
@@ -24,8 +46,25 @@ const Todo = () => {
 
         <div className='todoinputdata'>
           <input value={inputData} onChange={(e) => handleclick(e)} type="text" placeholder='Add item...' />
-          <i class="fa-sharp fa-solid fa-plus"></i>
+          <i class="fa-sharp fa-solid fa-plus" onClick={AddItem}></i>
         </div>
+
+        {addItem && addItem.map((e, index) => (
+          <div key={index} className='showTodoData'>
+            <div className='todoData'>
+              <div className='showData'>
+                <p style={{textAlign:"left"}}>{e}</p>
+              </div>
+              <i class="fa-solid fa-trash" onClick={() => deleteItem(index)}></i>
+            </div>
+          </div>
+        ))}
+
+        <div className='removeItem'>
+          <button onClick={RemoveAllTodo}>RemoveList</button>
+        </div>
+
+
 
       </div>
 
